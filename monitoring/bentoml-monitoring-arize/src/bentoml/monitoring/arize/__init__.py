ad056e82ab8e82797fa2427c2747d8d24e4ca400
bojiang <bojiang_@outlook.com>
2022-10-29 03:56:15 +0800

    finish

--- monitoring/bentoml-monitoring-arize/src/bentoml/monitoring/arize/__init__.py
+++ monitoring/bentoml-monitoring-arize/src/bentoml/monitoring/arize/__init__.py
@@ -4,30 +4,234 @@ import os
 import typing as t
 import logging
 import datetime
+import functools
 import collections
+from enum import Enum
+from enum import unique
 
 import attr
-from arize.api import Client  # type: ignore
-from arize.api import Embedding  # type: ignore
-from arize.api import ModelTypes  # type: ignore
-from arize.utils.types import Environments  # type: ignore
+from arize.api import Client
+from arize.utils.types import Embedding
+from arize.utils.types import ModelTypes
+from arize.utils.types import Environments
 
 from bentoml.monitoring import MonitorBase
 
-BENTOML_MONITOR_ROLES = {"feature", "prediction", "actual"}
+BENTOML_MONITOR_ROLES = {"feature", "prediction", "target"}
 BENTOML_MONITOR_TYPES = {"numerical", "categorical", "numerical_sequence"}
 logger = logging.getLogger(__name__)
-DataType: t.TypeAlias = str | int | float | bool
+DataType = t.Union[str, int, float, bool, t.List[float]]
+
+
+@unique
+class Mapping(Enum):
+    """
+    Mapping solutions for bentoml data fields to arize data fields
+    """
+
+    SCORED_CLASSIFICATION = 1
+    CLASSIFICATION = 2
+    REGRESSION = 3
+    RANKING = 4
 
 
 @attr.define(auto_attribs=True)
-class ArizeForm:
-    prediction_label_column: str | None = None
-    prediction_score_column: str | None = None
-    actual_label_column: str | None = None
-    actual_score_column: str | None = None
-    feature_columns: list[str] | None = None
-    embedding_feature_columns: list[str] | None = None
+class _FieldStats:
+    prediction_label_columns: list[str] = attr.field(factory=list)
+    prediction_score_columns: list[str] = attr.field(factory=list)
+    actual_label_columns: list[str] = attr.field(factory=list)
+    actual_score_columns: list[str] = attr.field(factory=list)
+    feature_columns: list[str] = attr.field(factory=list)
+    embedding_feature_columns: list[str] = attr.field(factory=list)
+
+
+def _stat_fields(schema: list[dict[str, str]]) -> _FieldStats:
+    fields = _FieldStats()
+    for column in schema:
+        if column["role"] == "feature":
+            if column["type"] == "numerical_sequence":
+                fields.embedding_feature_columns.append(column["name"])
+            else:
+                fields.feature_columns.append(column["name"])
+        elif column["type"] == "categorical" and column["role"] == "prediction":
+            fields.prediction_label_columns.append(column["name"])
+        elif column["type"] == "numerical" and column["role"] == "prediction":
+            fields.prediction_score_columns.append(column["name"])
+        elif column["type"] == "numerical_sequence" and column["role"] == "prediction":
+            logger.warning(
+                "Arize Monitor does not support numerical_sequence for prediction. "
+                "Ignoring column %s",
+                column["name"],
+            )
+        elif column["type"] == "categorical" and column["role"] == "target":
+            fields.actual_label_columns.append(column["name"])
+        elif column["type"] == "numerical" and column["role"] == "target":
+            fields.actual_score_columns.append(column["name"])
+        elif column["type"] == "numerical_sequence" and column["role"] == "target":
+            logger.warning(
+                "Arize Monitor does not support numerical_sequence for target. "
+                "Ignoring column %s",
+                column["name"],
+            )
+        else:
+            logger.warning(
+                "Arize Monitor does not support column %s with role %s and type %s."
+                "Ignoring column",
+                column["name"],
+                column["role"],
+                column["type"],
+            )
+    return fields
+
+
+def _is_valid_classification_form(fields: _FieldStats, warn: bool = False) -> bool:
+    if fields.prediction_label_columns and not fields.prediction_score_columns:
+        if warn and len(fields.prediction_label_columns) > 1:
+            logger.warning(
+                "Arize only supports single prediction label column, column %s will be ignore",
+                fields.prediction_label_columns[1:],
+            )
+        return True
+    if fields.actual_label_columns and not fields.actual_score_columns:
+        if warn and len(fields.actual_label_columns) > 1:
+            logger.warning(
+                "Arize only supports single actual label column, column %s will be ignore",
+                fields.actual_label_columns[1:],
+            )
+        return True
+    return False
+
+
+def _is_valid_scored_classification_form(
+    fields: _FieldStats, warn: bool = False
+) -> bool:
+    if fields.prediction_label_columns and fields.prediction_score_columns:
+        if warn and len(fields.prediction_label_columns) > 1:
+            logger.warning(
+                "Arize only supports single prediction label column, column %s will be ignore",
+                fields.prediction_label_columns[1:],
+            )
+        if warn and len(fields.prediction_score_columns) > 1:
+            logger.warning(
+                "Arize only supports single prediction score column, column %s will be ignore",
+                fields.prediction_score_columns[1:],
+            )
+        return True
+    if fields.actual_label_columns and fields.actual_score_columns:
+        if warn and len(fields.actual_label_columns) > 1:
+            logger.warning(
+                "Arize only supports single actual label column, column %s will be ignore",
+                fields.actual_label_columns[1:],
+            )
+        if warn and len(fields.actual_score_columns) > 1:
+            logger.warning(
+                "Arize only supports single actual score column, column %s will be ignore",
+                fields.actual_score_columns[1:],
+            )
+        return True
+    return False
+
+
+def _is_valid_regression_form(fields: _FieldStats, warn: bool = False) -> bool:
+    assert fields.is_filled
+    if fields.prediction_score_columns and not fields.prediction_label_columns:
+        if warn and len(fields.prediction_score_columns) > 1:
+            logger.warning(
+                "Arize only supports single prediction score column, column %s will be ignore",
+                fields.prediction_score_columns[1:],
+            )
+        return True
+    if fields.actual_score_columns and not fields.actual_label_columns:
+        if warn and len(fields.actual_score_columns) > 1:
+            logger.warning(
+                "Arize only supports single actual score column, column %s will be ignore",
+                fields.actual_score_columns[1:],
+            )
+        return True
+    return False
+
+
+def _infer_mapping(
+    fields: _FieldStats,
+    model_type: ModelTypes | None = None,
+) -> Mapping:
+    """
+    Infer the mapping solution for bentoml data fields to arize data fields
+    https://docs.arize.com/arize/model-schema-mapping#performance-metrics
+    """
+    if model_type is None:
+        if _is_valid_scored_classification_form(fields):
+            mapping = Mapping.SCORED_CLASSIFICATION
+        elif _is_valid_classification_form(fields):
+            mapping = Mapping.CLASSIFICATION
+        elif _is_valid_regression_form(fields):
+            mapping = Mapping.REGRESSION
+        else:
+            raise ValueError(
+                "failed to find a valid mapping to arize schema for the given schema. "
+                "Please specify a mapping using the `model_type` parameter."
+            )
+    elif model_type == ModelTypes.SCORE_CATEGORICAL:
+        if _is_valid_scored_classification_form(fields, warn=True):
+            mapping = Mapping.SCORED_CLASSIFICATION
+        elif _is_valid_classification_form(fields, warn=True):
+            mapping = Mapping.CLASSIFICATION
+        else:
+            raise ValueError("Not a valid arize classification schema")
+    elif model_type == ModelTypes.NUMERIC:
+        if _is_valid_regression_form(fields, warn=True):
+            mapping = Mapping.REGRESSION
+        else:
+            raise ValueError("Not a valid arize regression schema")
+    else:
+        logger.warning(
+            "Arize Monitor does not support model type %s. Falling back to default mapping"
+        )
+        mapping = Mapping.REGRESSION
+    return mapping
+
+
+_mapping_to_model_type = {
+    Mapping.SCORED_CLASSIFICATION: ModelTypes.SCORE_CATEGORICAL,
+    Mapping.CLASSIFICATION: ModelTypes.SCORE_CATEGORICAL,
+    Mapping.REGRESSION: ModelTypes.NUMERIC,
+}
+
+
+def _map_data(
+    record: dict[str, DataType], fields: _FieldStats, mapping: Mapping
+) -> tuple:
+    """
+    Map bentoml monitoring record to arize fields
+    """
+    if mapping == Mapping.SCORED_CLASSIFICATION:
+        prediction_label = (
+            record[fields.prediction_label_columns[0]],
+            record[fields.prediction_score_columns[0]],
+        )
+        actual_label = (
+            record[fields.actual_label_columns[0]],
+            record[fields.actual_score_columns[0]],
+        )
+    elif mapping == Mapping.CLASSIFICATION:
+        prediction_label = record[fields.prediction_label_columns[0]]
+        actual_label = record[fields.actual_label_columns[0]]
+    elif mapping == Mapping.REGRESSION:
+        prediction_label = record[
+            (fields.prediction_score_columns + fields.prediction_label_columns)[0]
+        ]
+        actual_label = record[
+            (fields.actual_score_columns + fields.actual_label_columns)[0]
+        ]
+    else:
+        logger.warning("Mapping not supported. Fallback to regression")
+        prediction_label = record[fields.prediction_score_columns[0]]
+        actual_label = record[fields.actual_score_columns[0]]
+    features = {c: record[c] for c in fields.feature_columns}
+    embedding_features = {
+        c: Embedding(vector=record[c]) for c in fields.embedding_feature_columns  # type: ignore
+    }
+    return prediction_label, actual_label, features, embedding_features
 
 
 class ArizeMonitor(MonitorBase[DataType]):
@@ -47,8 +251,8 @@ class ArizeMonitor(MonitorBase[DataType]):
         model_type: ModelTypes | None = None,
         model_id: str | None = None,
         model_version: str | None = None,
-        model_environment: Environments | None = None,
-        model_tags: t.Dict[str, str] | None = None,
+        environment: Environments | None = None,
+        model_tags: dict[str, str | bool | float | int] | None = None,
     ):
         self.name = name
 
@@ -70,12 +274,11 @@ class ArizeMonitor(MonitorBase[DataType]):
         self.model_type = model_type
         self.model_id = model_id
         self.model_version = model_version
-        self.model_environment = model_environment
+        self.environment = environment
         self.model_tags = model_tags
 
-        self._form = ArizeForm()
-
         # internal state
+        self._is_recording = False
         self._is_first_record = True
         self._is_first_column = False
         self._schema: list[dict[str, str]] = []
@@ -94,26 +297,6 @@ class ArizeMonitor(MonitorBase[DataType]):
             max_queue_bound=self.max_queue_bound,
             timeout=self.timeout,
         )
-
-    def _infer_model_type(self):
-        """
-        https://docs.arize.com/arize/model-schema-mapping#performance-metrics
-        """
-        assert self._schema
-        numeric_flag = 0
-        categorical_flag = 0
-        for column in self._schema:
-            if column["type"] == "categorical" and column["role"] == "prediction":
-                categorical_flag += 1
-            elif column["type"] == "numerical" and column["role"] == "prediction":
-                numeric_flag += 1
-        if categorical_flag == 1 and numeric_flag >= 1:
-            return ModelTypes.SCORE_CATEGORICAL
-        if categorical_flag == 1 and numeric_flag == 0:
-            return ModelTypes.SCORE_CATEGORICAL
-        if numeric_flag:
-            return ModelTypes.NUMERIC
-        raise ValueError("Cannot infer model type from schema")
 
     def start_record(self) -> None:
         """
@@ -136,20 +319,27 @@ class ArizeMonitor(MonitorBase[DataType]):
 
     def export_schema(self):
         """
-        Export schema of the data. This method should be called after all data is logged.
+        Export schema of the data. This method should be called right after the first record.
         """
+        fields = _stat_fields(self._schema)
+        mapping = _infer_mapping(fields, self.model_type)
+        self._data_converter = functools.partial(
+            _map_data, fields=fields, mapping=mapping
+        )
+
         if self.model_type is None:
-            self.model_type = self._infer_model_type()
+            self.model_type = _mapping_to_model_type[mapping]
+
         if self.model_version is None and self.model_id is None:
             from bentoml._internal.context import component_context
 
             self.model_id = component_context.bento_name
             self.model_version = component_context.bento_version
-        if self.model_environment is None:
-            self.model_environment = Environments.PRODUCTION
 
-    def _yield_arize_data(self):
-        pass
+        if self.environment is None:
+            self.environment = Environments.PRODUCTION
+
+        self._init_client()
 
     def export_data(self):
         """
@@ -158,23 +348,30 @@ class ArizeMonitor(MonitorBase[DataType]):
         assert (
             len(set(len(q) for q in self._columns.values())) == 1
         ), "All columns must have the same length"
+        assert self.model_id is not None
+        assert self.model_type is not None
+        assert self.environment is not None
         while True:
             try:
                 record = {k: v.popleft() for k, v in self._columns.items()}
+                timestamp = record[self.COLUMN_TIME]
+                assert isinstance(timestamp, float)
+                prediction_id = record[self.COLUMN_RID]
+                assert isinstance(prediction_id, int)
+                data_infos = self._data_converter(record)
                 self._client.log(
                     model_id=self.model_id,
                     model_type=self.model_type,
-                    environment=self.model_environment,
+                    environment=self.environment,
                     model_version=self.model_version,
                     tags=self.model_tags,
-                    prediction_id=record[self.COLUMN_RID],
-                    prediction_timestamp=record[self.COLUMN_TIME],
+                    prediction_id=prediction_id,
+                    prediction_timestamp=int(timestamp),
                     batch_id=None,
-                    prediction_label: Union[str, bool, int, float, Tuple[str, float]] = None,
-                    actual_label: Union[str, bool, int, float, Tuple[str, float]] = None,
-                    features: Optional[Dict[str, Union[str, bool, float, int]]] = None,
-                    embedding_features: Optional[Dict[str, Embedding]] = None,
-                    shap_values: Dict[str, float] = None,
+                    prediction_label=data_infos[0],
+                    actual_label=data_infos[1],
+                    features=data_infos[2],
+                    embedding_features=data_infos[3],
                 )
             except IndexError:
                 break
@@ -231,7 +428,7 @@ class ArizeMonitor(MonitorBase[DataType]):
 
     def log_table(
         self,
-        data: t.Iterable[t.Iterable[DataType]],
-        schema: dict[str, str],
+        data: t.Any,  # type: pandas.DataFrame
+        schema: list[dict[str, str]],
     ) -> None:
         raise NotImplementedError("Not implemented yet")
